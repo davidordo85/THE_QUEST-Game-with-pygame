1895edbb6361c9ee6e34d503fdca2a039e1548b7
David Ordoñez <davidordonezescacena@hotmail.es>
2023-03-25 17:41:40 +0100

    created game loop

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print('start')
+import pygame as pg
+import sys
+
+class Main():
+    def __init__(self, w, h):
+        self.display = pg.display.set_mode((h, w))
+        pg.display.set_caption("THE QUEST")
+        running = True
+
+    def main_loop(self):
+        while True:
+            for event in pg.event.get():
+                if event.type == pg.QUIT:
+                    return self.quit()
+            self.display.fill("red")
+            pg.display.flip()
+    
+
+    def quit(self):
+        pg.quit()
+        sys.exit()
+
+if __name__ == '__main__':
+    pg.init(), 
+    game = Main(600, 1000)
+    game.main_loop()
+    game.quit()
