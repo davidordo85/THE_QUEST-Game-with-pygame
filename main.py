2b51967e139cba9834df8c6458e9e1f10e7f7419
David Ordoñez <davidordonezescacena@hotmail.es>
2023-03-27 13:09:07 +0200

    screen movement in first level, added punctuation ship and asteroid

--- main.py
+++ main.py
@@ -7,29 +7,36 @@ import time
 BLACK = (0, 0, 0)
 WHITE = (255, 255, 255)
 
-""" self.handleEvent()
-            self.ship.update(800, 600)
-            self.asteroid.update(800, 600)
-            self.display.fill("black")
-            self.display.blit(self.ship.image, self.ship.rect)
-            self.display.blit(self.asteroid.image, self.asteroid.rect) """
-
 class Main():
     clock = pg.time.Clock()
     def __init__(self, w, h):
+        #create the screen with its size
         self.screen = pg.display.set_mode((h, w))
-        self.status = 'Opening'
+        #name the window
         pg.display.set_caption("THE QUEST")
-        self.title = pg.font.Font('./resources/fonts/OdibeeSans-Regular.ttf', 100)
+
+        # added fonts and images
         self.description = pg.font.Font('./resources/fonts/OdibeeSans-Regular.ttf', 30)
+        self.title = pg.font.Font('./resources/fonts/OdibeeSans-Regular.ttf', 100)
+        self.fontScore = pg.font.Font('./resources/fonts/OdibeeSans-Regular.ttf', 50)
+        self.background = pg.image.load("./resources/images/backgrounds/background_game.png")
+
+        self.score = 0
+        self.status = 'Opening'
+
+        # create the screen texts and objects
         self.displayProduction = self.title.render("GERMEN PRODUCTION", True, (WHITE))
         self.displayOpening = self.title.render("THE QUEST", True, (WHITE))
         self.displayDescription = self.description.render("To avoid colliding with the asteroids use the up or down keys, until the planet to land on appears", True, (WHITE))
         self.startGame = self.description.render("Press space for start", True, (WHITE))
+        self.displayScore = self.fontScore.render(str(self.score), True, WHITE)
         self.ship = ship.Ship(800, 600)
         self.asteroid = asteroids.Asteroid(800, 600)
 
-    def handleEvent(self): # create the keyboard events
+        
+
+    # create the keyboard events
+    def handleEvent(self): 
         self.start = False
         for event in pg.event.get():            
             if event.type == pg.QUIT:
@@ -44,11 +51,24 @@ class Main():
                     self.start = True             
             elif event.type == pg.KEYUP:
                 self.ship.vy = 0
-            
             else:
-                pass        
+                pass
+        key_pressed = pg.key.get_pressed()
+        if key_pressed[pg.K_UP]:
+            self.ship.vy -= 1
+                
+        elif key_pressed[pg.K_DOWN]:
+            self.ship.vy += 1
+        else:
+            self.ship.vy = 0   
 
         return False
+
+    def backgroundMove(self):
+        self.x -= 0.5
+        if self.x <= -2400:
+            self.x = 0
+        
 
     def redraw(self):
         pg.display.flip()
@@ -59,8 +79,6 @@ class Main():
             self.handleEvent()
             self.screen.fill("black")
             self.screen.blit(self.displayProduction, (150, 200))
-            
-            self.ship.update(800, 600)
             self.redraw()
             time.sleep(4)
             displayOpening = True
@@ -72,11 +90,9 @@ class Main():
         while not front:
             self.handleEvent()            
             self.screen.fill("black")
-            self.ship.update(800, 600)
             self.screen.blit(self.displayOpening, (100, 50))
             self.screen.blit(self.displayDescription, (150, 250))
             self.screen.blit(self.startGame, (150, 500))
-            self.screen.blit(self.ship.image, self.ship.rect)
             self.redraw()
             if self.start == True:
                 front = True
@@ -84,9 +100,21 @@ class Main():
         self.status = 'First_level'
 
     def firstLevel(self):
-        self.handleEvent()
-        self.screen.fill("white")
-        self.redraw()
+        first = False
+        self.x = 0
+        
+
+        while not first:
+            self.handleEvent()
+            self.backgroundMove()
+            self.ship.update(800, 600)
+            self.asteroid.update(800, 600)
+            self.screen.blit(self.background, (self.x, 0))
+            self.screen.blit(self.background, (self.x+2400, 0))
+            self.screen.blit(self.displayScore, (950, 30))
+            self.screen.blit(self.ship.image, self.ship.rect)
+            self.screen.blit(self.asteroid.image, self.asteroid.rect)
+            self.redraw()
 
         
 
